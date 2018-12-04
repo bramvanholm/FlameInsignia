c6fac0d0c2b3484395566f4641db75f5f766f33d
bramvanholm <45210562+bramvanholm@users.noreply.github.com>
2018-12-04 23:08:41 +0100

    Add files via upload

--- Flame Insignia/src/gui/GameGUI.java
+++ Flame Insignia/src/gui/GameGUI.java
@@ -3,21 +3,11 @@ package gui;
 //Temp storage of imports; divide/remove this later
 
 import java.awt.*;
-import java.awt.GridLayout;
-import java.awt.Dimension;
-
 import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JProgressBar;
-import javax.swing.JList;
-import javax.swing.JRadioButton;
 
 import game.Unit;
 import game.Ally;
@@ -27,18 +17,15 @@ import game.Enemy;
 
 public class GameGUI extends JFrame {
 
-	//BL: Interesting Classes: JLayered, JTextPane
-
-
-
 
 	JPanel gridPanel = new JPanel();
-
-
 	JPanel infoPanel = new JPanel();
 	JPanel turnPanel = new JPanel();
 	JPanel statPanel = new JPanel();
 
+
+	Font font = new Font("Calibri",Font.PLAIN, 16);
+	
 	String f = "F";
 	//BL: Search random selection of array
 	private static void addButtons(Container contentPane,
@@ -47,22 +34,62 @@ public class GameGUI extends JFrame {
 			contentPane.add(new JButton(label));
 		}
 	}
-
-	private static void addLabels(Container contentPane, String ... labels) {
-		for(String label : labels) {
-			contentPane.add(new JLabel(label));
+	
+	private static void addInfo(Container contentPane, Font font, Unit ... units) {
+		for(Unit unit : units) {
+			JPanel panel = new JPanel();
+			JLabel info = new JLabel(unit.getInfo());
+			JLabel stats = new JLabel(unit.getStats());
+			JProgressBar progressBar = new JProgressBar(0,unit.getMAXHP());
+			
+			info.setFont(font);
+			stats.setFont(font);
+			progressBar.setValue(unit.getHP());
+			
+			panel.setLayout(new GridLayout(2,1));
+			panel.add(info);
+			panel.add(progressBar);
+			panel.add(stats);
+			panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+			contentPane.add(panel);
 		}
 	}
 
+	// BL: make method to set/raise stats
+	private static void setEnemyStats (int maxhp, int atk, Enemy ... enemies) {
+		for (Enemy enemy : enemies) {
+			enemy.setMAXHP(maxhp);
+			enemy.setHP(maxhp);
+			enemy.setATK(atk);
+		}
+	}
+	
+	
 	GameGUI(String difficulty, String princeStat) {
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Flame Insignia: Battle");
-
-		Enemy boss = new Enemy("Bandit Leader",30,75,8,7,0);
-		Ally retainer = new Ally("Retainer", 22,75,8,5);
-		Ally squire = new Ally("Squire", 22,75,7,6);
+		Enemy boss = new Enemy("Bandit Leader",25,75,9,6,0);
+		Enemy bandit1 = new Enemy("Bandit 1",13,70,8,5);
+		Enemy bandit2 = new Enemy("Bandit 2",13,70,8,5);
+		Enemy bandit3 = new Enemy("Bandit 3",13,70,8,5);
+		Enemy bandit4 = new Enemy("Bandit 4",13,70,8,5);
+		
+		Ally guard1 = new Ally("Guard 1", 22,75,6,6);
+		Ally guard2 = new Ally("Guard 2", 22,75,6,6);
 		Ally prince = new Ally("Prince",25,75,7,5);
+
+		switch(difficulty) {
+		case "Easy":
+			break;
+		case "Standard": 
+			setEnemyStats(18,1, bandit1, bandit2, bandit3, bandit4);
+			setEnemyStats(30,1,boss);
+			break;
+		case "Hard":
+			setEnemyStats(21,2,bandit1, bandit2, bandit3, bandit4);
+			setEnemyStats(35,2,boss);
+			break;
+		}
+		
 		switch (princeStat) {
 		case "HIT": prince.setHIT(15);
 		break;
@@ -71,62 +98,27 @@ public class GameGUI extends JFrame {
 		case "DEF":prince.setDEF(2);
 		break;
 		}
-
-		statPanel.setLayout(new GridLayout(9,2));
 		
-		addLabels(statPanel,prince.getInfo(), prince.getStats());
-		addLabels(statPanel,retainer.getInfo(), retainer.getStats());
-		addLabels(statPanel,squire.getInfo(), squire.getStats());
-		addLabels(statPanel,boss.getInfo(), boss.getStats());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Flame Insignia: Battle");
 
-		Enemy bandit1 = null;
-		Enemy bandit2 = null;
-		Enemy bandit3 = null;
-		Enemy bandit4 = null;
-		Enemy bandit5 = null;
-		switch(difficulty) {
-			case "Easy": bandit1 = new Enemy("Bandit 1",15,70,9,5);
-				bandit2 = new Enemy("Bandit 2",15,70,9,5);
-				bandit3 = new Enemy("Bandit 3",15,70,9,5);
-				addLabels(statPanel,bandit1.getInfo(), bandit1.getStats());
-				addLabels(statPanel,bandit2.getInfo(), bandit2.getStats());
-				addLabels(statPanel,bandit3.getInfo(), bandit3.getStats());
-				break;
-				
-			case "Standard": bandit1 = new Enemy("Bandit 1",15,70,9,5);
-				bandit2 = new Enemy("Bandit 2",15,70,9,5);
-				bandit3 = new Enemy("Bandit 3",15,70,9,5);
-				bandit4 = new Enemy("Bandit 4",15,70,9,5);
-				addLabels(statPanel,bandit1.getInfo(), bandit1.getStats());
-				addLabels(statPanel,bandit2.getInfo(), bandit2.getStats());
-				addLabels(statPanel,bandit3.getInfo(), bandit3.getStats());
-				addLabels(statPanel,bandit4.getInfo(), bandit4.getStats());
-				break;
-			case "Hard": bandit1 = new Enemy("Bandit 1",15,70,9,5);
-				bandit2 = new Enemy("Bandit 2",15,70,9,5);
-				bandit3 = new Enemy("Bandit 3",15,70,9,5);
-				bandit4 = new Enemy("Bandit 4",15,70,9,5);
-				bandit5 = new Enemy("Bandit 5",15,70,9,5);
-				addLabels(statPanel,bandit1.getInfo(), bandit1.getStats());
-				addLabels(statPanel,bandit2.getInfo(), bandit2.getStats());
-				addLabels(statPanel,bandit3.getInfo(), bandit3.getStats());
-				addLabels(statPanel,bandit4.getInfo(), bandit4.getStats());
-				addLabels(statPanel,bandit5.getInfo(), bandit5.getStats());
-		}
-		statPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-
+		statPanel.setLayout(new GridLayout(16,2));
+		addInfo(statPanel, font, prince, guard1, guard2, boss, bandit1, bandit2, bandit3, bandit4);
+		
+		
 		gridPanel.setLayout(new GridLayout(8,8));
 		addButtons(gridPanel,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f);
 		// Random selection from array, for loop for assigning buttons
 
-		JLabel turnLabel = new JLabel("Turns left: " +10, JLabel.LEFT); // Adjust to actual turns left
+		
+		JLabel turnLabel = new JLabel("Turns left: " +10); //BL: Adjust to actual turns left
+		turnLabel.setFont(font);
 		turnPanel.add(turnLabel);
 		turnPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-
-		infoPanel.setLayout(new GridLayout(2,1));
-		infoPanel.add(turnPanel);
-		infoPanel.add(statPanel);
+		infoPanel.setLayout(new BorderLayout());
+		infoPanel.add(turnPanel, BorderLayout.NORTH);
+		infoPanel.add(statPanel, BorderLayout.CENTER);
 
 		setLayout(new BorderLayout());
 		add(gridPanel, BorderLayout.CENTER);
